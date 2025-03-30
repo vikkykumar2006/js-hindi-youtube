fc3a4436172807bcdd96740a04616723bbe56a80
vikkykumar2006 <kumarvikkykumar84@gmail.com>
2025-03-30 05:28:58 +0000

    datatype

---
+++ bsicjs/dataype.js
@@ -0,0 +1,21 @@
+ //example of primitive datatype
+
+
+ let numbertype=34  //return type number
+ let numbertype1=34.4  //return type number
+ let stringtype="vikky"  // return type string
+ let bolleantype=true  // return type bollean
+ let bolleantype1=false  //return type bollean
+ let biginttype=23347948940398344747547n   // return type bigint
+ let nulltype=null//return type object
+ let undefinetype // return type object
+ console.log(typeof biginttype)
+ console.table([numbertype,numbertype1,stringtype,bolleantype,bolleantype1,biginttype,nulltype,undefinetype])
+ let array1=["vikky","ritesh","rikky",9]
+ let myobjt=
+ {
+    name:"vikky",
+    age:22
+ }
+ console.log(array1)
+ console.log(myobjt)

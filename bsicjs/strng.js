6a5c843183e0e2e07fefee94a07972de83886d92
vikkykumar2006 <kumarvikkykumar84@gmail.com>
2025-03-30 07:39:06 +0000

    string and method

---
+++ bsicjs/strng.js
@@ -0,0 +1,31 @@
+/*
+stringe ="Allenhouse institute of technology"
+const strgobj=new String('allenhouse')
+console.log(strgobj.charAt(5))
+console.log(strgobj.toUpperCase())
+console.log(strgobj.toLowerCase())
+*/
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*
+const s1 = "2" + "2"; // creates a string primitive
+const s2 = new String("2" + "2"); // creates a String object
+console.log(s1); // returns the 22 
+console.log(s2); // returns the string '22'
+string2="the way we try"
+console.log(string2.charAt(5))
+console.log(string2.toUpperCase())
+console.log(string2.toLowerCase())
+string3="            i am  hardworking                 "
+console.log(string3.trim())// ignore whitespace in start and end
+console.log(string3.trimEnd())// ignore whitespace end
+console.log(string3.trimStart())// ignore whitespace start
+*/
+string4="i have great parents"
+console.log(string4.split())// 
+console.log(string4.replace("i","we"))
+console.log(string4.slice(-8,-1)) 
+console.log(string4.slice(-8,-1)) 
+console.log(string4.substring(0,8))
+
+
+

b2d35db2dc74da487620a96e6f1bd56864fd3af3
vikkykumar2006 <kumarvikkykumar84@gmail.com>
2025-03-31 08:24:26 +0000

    array and its prty

---
+++ bsicjs/array1.js
@@ -0,0 +1,34 @@
+/*
+const arry1=[1,2,3,4,5]
+console.log(arry1)
+arry1.push(6)
+arry1.push(7)
+console.log(arry1)
+arry1.pop()
+console.log(arry1)
+
+
+output---------------
+[ 1, 2, 3, 4, 5 ]
+[
+  1, 2, 3, 4,
+  5, 6, 7
+]
+[ 1, 2, 3, 4, 5, 6 ]
+*/
+const array2=new Array(10,2,3,4,5,6,6)
+console.log(array2)
+const array3 = array2.join()
+console.log(array3)
+console.log(array2.includes(8));
+console.log(array2.indexOf(5));
+const arrasplc=array2.splice(1,6)
+console.log(array2)
+console.log(array2.splice(1,6));
+console.log(array2)
+
+
+
+
+
+
